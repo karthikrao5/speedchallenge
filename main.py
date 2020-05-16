40b3e047136474003623412f58deaab469bdedb2
Karthik Rao <karthik.rao543@gmail.com>
2020-05-16 15:13:06 -0400

    training and validation set

--- main.py
+++ main.py
@@ -1,38 +1,116 @@
 import cv2
 
-from googleObjectDetection import GoogleObjectDetection
+import tensorflow as tf
+from tensorflow import keras
 
-googleInference = GoogleObjectDetection()
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
+# cap = cv2.VideoCapture("data/train.mp4")
+
+
+
+# # Check if camera opened successfully
+# if not cap.isOpened():
+#     print("Error opening video stream or file")
+
+# # Read until video is completed
+# while cap.isOpened():
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     if ret:
+        
+#         # edges = cv2.Canny(frame, 20, 200)
+
+#         # Display the resulting frame
+#         cv2.imshow('Frame', frame)
+
+#         # Press Q on keyboard to  exit
+#         if cv2.waitKey(25) & 0xFF == ord('q'):
+#             break
+
+#     # Break the loop
+#     else:
+#         break
+# # When everything done, release the video capture object 
+# cap.release()
+
+# # Closes all the frames
+# cv2.destroyAllWindows()
+
+images = []
+
 cap = cv2.VideoCapture("data/train.mp4")
 
-# Check if camera opened successfully
-if not cap.isOpened():
-    print("Error opening video stream or file")
+pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+while True:
+    flag, frame = cap.read() # get the frame
+    if flag:
+        # The frame is ready and already captured
+        # cv2.imshow('video', frame)
 
-# Read until video is completed
-while cap.isOpened():
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret:
-        
-        # edges = cv2.Canny(frame, 20, 200)
+        # store the current frame in as a numpy array
+        images.append(cv2.cvtColor(cv2.resize(frame, (100, 100)) , cv2.COLOR_BGR2GRAY))
 
-        # Display the resulting frame
-        cv2.imshow('Frame', googleInference.show_inference(frame))
-
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-
-    # Break the loop
+        # pos_frame = cap.get(cv2.cv.CAP_PROP_POS_FRAMES)
+        pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
     else:
+        # The next frame is not ready, so we try to read it again
+        cap.set(cv2.cv.CAP_PROP_POS_FRAMES, pos_frame-1)
+        print ("frame is not ready")
+        # It is better to wait for a while for the next frame to be ready
+        cv2.waitKey(1000)
+
+    if cv2.waitKey(10) == 27:
+        break
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        # If the number of captured frames is equal to the total number of frames,
+        # we stop
         break
 
-# When everything done, release the video capture object 
-cap.release()
+images = np.array(images)
 
-# Closes all the frames
-cv2.destroyAllWindows()
+print("total images length: %d" % (len(images)))
+
+train_num = int(len(images) * 0.8)
+print ("train set length: %d" % (train_num))
+
+train_images, test_images = images[:train_num,:], images[train_num:,:]
+
+print("train images: %d, test images: %d" % (len(train_images), len(test_images)))
+
+labels = []
+
+with open('data/train.txt', 'r') as f:
+    for line in f:
+        labels.append(float(line))
+
+print("label length: %d" % (len(labels)))
+print("train labels length: %d" % (train_num))
+
+train_labels = labels[:16320]
+test_labels = labels[16320:]
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(100, 100)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(1)
+])
+
+optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+model.compile(optimizer=optimizer,
+              loss='mse',
+              metrics=['mae', 'mse'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
